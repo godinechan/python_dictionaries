6e4e66aec951b7d672429cf00cdc9b18a946c814
Godine Chan <31738426+godinechan@users.noreply.github.com>
2023-01-08 23:42:19 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Create a dictionary
+my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
+
+# Access an element using the key
+print(my_dict['name'])  # Output: 'John'
+
+# Access an element using the get() method
+print(my_dict.get('age'))  # Output: 30
+
+# Modify an element in the dictionary
+my_dict['age'] = 35
+
+# Print the modified dictionary
+print(my_dict)  # Output: {'name': 'John', 'age': 35, 'city': 'New York'}
